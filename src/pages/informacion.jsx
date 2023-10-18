2a8a66baa993339305664f3b22172efcce122b67
Brandon <brandon.monsalve@sansano.usm.cl>
2023-10-18 19:38:59 -0300

    Test

--- src/pages/informacion.jsx
+++ src/pages/informacion.jsx
@@ -4,16 +4,16 @@ import { Button } from '@mui/material'
 import NavBar from '../components/nav_bar'
 import LogoUSM from '../assets/usm.png'
 
-export const InternalPage = () => {
+export const PaginaDeInfo = () => {
   return (
     <div className='page'>
-      <h1 className='page__title'>Frontend Sample App</h1>
+      <h1 className='page__title'>Reciclaje Inteligente</h1>
       <NavBar />
-      <h2>Página Interna</h2>
+      <h2>Información</h2>
       <img src={LogoUSM} height={100} alt='logo usm' /><br />
       <Button variant='contained'>Material UI Button</Button>
     </div>
   )
 }
 
-export default InternalPage
+export default PaginaDeInfo
