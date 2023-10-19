b62ba3982d4772069a06385360caf083c1be33b0
Brandon <brandon.monsalve@sansano.usm.cl>
2023-10-19 20:30:28 -0300

    Información y Favoritos

--- src/pages/favorito.jsx
+++ src/pages/favorito.jsx
@@ -1,19 +1,70 @@
-import React from 'react'
-import { Button } from '@mui/material'
-
-import NavBar from '../components/nav_bar'
-import LogoUSM from '../assets/usm.png'
+import React, { useState } from 'react';
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import NavBar from '../components/nav_bar';
+import PruebaImagen from '../assets/prueba.png'
 
 export const PaginaDeFavoritos = () => {
+
+  const [contenedoresFavoritos, setContenedoresFavoritos] = useState([]);
+
+  const paperStyle = {
+    padding: '16px',
+    textAlign: 'left',
+    color: '#000',
+    marginLeft: '10px',
+    marginRight: '10px',
+    marginTop: '10px',
+    marginBottom: '10px'
+  };
+
+  const agregarContenedorTemporal = () => {
+    // Aquí puedes agregar la lógica para añadir un contenedor a la lista de favoritos
+    // Simularemos esto añadiendo un contenedor de prueba
+    const contenedorPrueba = {
+      nombre: 'Contenedor de Prueba',
+      lugar: 'Ubicación de Prueba',
+      disponibilidad: 'Disponible',
+      imagenSrc: PruebaImagen, // Añade la URL de la imagen de prueba
+    };
+
+    setContenedoresFavoritos([...contenedoresFavoritos, contenedorPrueba]);
+  };
+
   return (
     <div className='page'>
       <h1 className='page__title'>Reciclaje Inteligente</h1>
       <NavBar />
-      <h2>Favoritos</h2>
-      <img src={LogoUSM} height={100} alt='logo usm' /><br />
-      <Button variant='contained'>Material UI Button</Button>
+      <div style={{ textAlign: 'center' }}>
+        <h2>Favoritos</h2>
+      </div>
+
+      <div style={{textAlign: 'center', marginTop: '10px'}}>
+        <Button variant='contained' onClick={agregarContenedorTemporal} >
+          Agregar Contenedor Temporal
+        </Button>
+      </div>
+
+      {/* Mostrar contenedores favoritos */}
+      {contenedoresFavoritos.map((contenedor, index) => (
+        <Paper key={index} style={paperStyle}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+              <img
+                src={contenedor.imagenSrc}
+                alt={`Contenedor de ${contenedor.nombre}`}
+                style={{ maxWidth: '20%', height: 'auto' }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4">{contenedor.nombre}</Typography>
+              <Typography variant="body1">Lugar: {contenedor.lugar}</Typography>
+              <Typography variant="body1">Disponibilidad: {contenedor.disponibilidad}</Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      ))}
     </div>
-  )
+  );
 }
 
 export default PaginaDeFavoritos
