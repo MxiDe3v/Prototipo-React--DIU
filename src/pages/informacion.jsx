cd3962b146fac284f6592bbe904638e93a029397
Daniel-Parraguez <daniel.parraguezs@usm.cl>
2023-11-13 19:02:49 -0300

    Informacion punteada

--- src/pages/informacion.jsx
+++ src/pages/informacion.jsx
@@ -7,7 +7,6 @@ import ContenedorVerdeImagen from '../assets/contenedor_verde.png';
 import ContenedorGrisImagen from '../assets/contenedor_gris.png';
 import ContenedorRojoImagen from '../assets/contenedor_rojo.png';
 import ContenedorNaranjoImagen from '../assets/contenedor_naranjo.png';
-import Logo from "../assets/logo.png"
 import Footer from '../components/footer'
 
 const ContenedorInfo = ({ titulo, contenido, imagenSrc }) => {
@@ -45,6 +44,8 @@ const ContenedorInfo = ({ titulo, contenido, imagenSrc }) => {
     padding: '1.5rem'
   };
 
+  const elementosContenido = contenido.split('\n');
+
   return (
     <Paper style={paperStyle}>
       <Grid container spacing={0} style={containerStyle}>
@@ -53,7 +54,13 @@ const ContenedorInfo = ({ titulo, contenido, imagenSrc }) => {
         </Grid>
         <Grid item xs={12} md={6} style={textContainerStyle}>
           <Typography variant="h4" style={{fontWeight: 'bold'}}>{titulo}</Typography>
-          <Typography variant="body1">{contenido}</Typography>
+          <ul>
+            {elementosContenido.map((elemento, index) => 
+              <li key={index}>
+                <Typography variant="body1">{elemento}</Typography>
+              </li>
+            )}
+          </ul>
         </Grid>
       </Grid>
     </Paper>
@@ -67,56 +74,59 @@ export const PaginaDeInfo = () => {
       
       <ContenedorInfo
         titulo="Contenedor Azul"
-        contenido="Contenedores azules para papel y cartón. 
-        Asegúrate de que estén limpios y plegados para facilitar el reciclaje. 
-        Elimina contaminantes como alimentos y químicos. 
-        Pliega cartones y papel para ahorrar espacio. 
-        No incluyas papeles encerados, papel higiénico o papel de aluminio."
+        contenido={`Contenedores azules para papel y cartón.\n 
+        Asegúrate de que estén limpios y plegados para facilitar el reciclaje.\n 
+        Elimina contaminantes como alimentos y químicos.\n 
+        Pliega cartones y papel para ahorrar espacio.\n 
+        No incluyas papeles encerados, papel higiénico o papel de aluminio.`}
         imagenSrc={ContenedorAzulImagen}
       />
 
       <ContenedorInfo
         titulo="Contenedor Verde"
-        contenido="Contenedores verdes para vidrio. 
-        Enjuaga y seca el vidrio antes de depositarlo para eliminar residuos.
-        Retira tapas y etiquetas antes de reciclar.
-        No incluyas vidrio plano, cerámica o cristal en este contenedor."
+        contenido={`Contenedores verdes para vidrio.\n  
+        Enjuaga y seca el vidrio antes de depositarlo para eliminar residuos.\n 
+        Retira tapas y etiquetas antes de reciclar.\n 
+        No incluyas vidrio plano, cerámica o cristal en este contenedor.`}
         imagenSrc={ContenedorVerdeImagen}
       />
 
       <ContenedorInfo
         titulo="Contenedor Rojo"
-        contenido="Contenedores rojos para residuos peligrosos como baterías, pilas, aceites o medicamentos. Estos materiales requieren gestión cuidadosa debido a sus características peligrosas. 
-        Almacena baterías y pilas sin agujeros o deformidades.
-        Mantener medicamentos separados para evitar reacciones.
-        Recicla aceites no quemados en contenedores específicos."
+        contenido={`Contenedores rojos para residuos peligrosos como baterías, pilas, aceites o medicamentos.\n  
+        Estos materiales requieren gestión cuidadosa debido a sus características peligrosas.\n  
+        Almacena baterías y pilas sin agujeros o deformidades.\n 
+        Mantener medicamentos separados para evitar reacciones.\n 
+        Recicla aceites no quemados en contenedores específicos.`}
         imagenSrc={ContenedorRojoImagen}
       />
 
       <ContenedorInfo
         titulo="Contenedor Amarillo"
-        contenido="Contenedores amarillos para plásticos. 
-        Lava y seca los envases plásticos antes de depositarlos para eliminar los residuos.
-        Separa tapones y etiquetas antes de reciclar.
-        Evita desechar plásticos no reciclables, como juguetes o utensilios de cocina."
+        contenido={`Contenedores amarillos para plásticos.\n  
+        Lava y seca los envases plásticos antes de depositarlos para eliminar los residuos.\n 
+        Separa tapones y etiquetas antes de reciclar.\n 
+        Evita desechar plásticos no reciclables, como juguetes o utensilios de cocina.`}
         imagenSrc={ContenedorAmarilloImagen}
       />
 
       <ContenedorInfo
         titulo="Contenedor Gris"
-        contenido="Contenedores grises para metales. 
-        Asegúrate de que los envases metálicos estén limpios y libres de alimentos o residuos.
-        Separa los diferentes tipos de metales, como aluminio y acero, para facilitar el reciclaje.
-        Evita desechar objetos metálicos grandes o contaminados con productos químicos."
+        contenido={`Contenedores grises para metales.\n  
+        Asegúrate de que los envases metálicos estén limpios y libres de alimentos o residuos.\n 
+        Separa los diferentes tipos de metales, como aluminio y acero, para facilitar el reciclaje.\n 
+        Evita desechar objetos metálicos grandes o contaminados con productos químicos.`}
         imagenSrc={ContenedorGrisImagen}
       />
 
       <ContenedorInfo
         titulo="Contenedor Naranjo"
-        contenido="Contenedor para restos de alimentos destinados a compost. Incluye pieles de frutas, espinas de pescado, plantas, cáscaras de huevo y posos. Estos restos se utilizarán para hacer compost, un fertilizante y generador de energía.
-        Separa los restos de alimentos de otros desechos no compostables.
-        No incluyas plásticos, papeles ni materiales no orgánicos en este contenedor.
-        Utiliza el compost producido en jardines o para fertilizar plantas."
+        contenido={`Contenedor para restos de alimentos destinados a compost.\n  
+        Incluye pieles de frutas, espinas de pescado, plantas, cáscaras de huevo y posos.\n  
+        Estos restos se utilizarán para hacer compost, un fertilizante y generador de energía.\n 
+        Separa los restos de alimentos de otros desechos no compostables.\n 
+        No incluyas plásticos, papeles ni materiales no orgánicos en este contenedor.\n 
+        Utiliza el compost producido en jardines o para fertilizar plantas.`}
         imagenSrc={ContenedorNaranjoImagen}
       />
     <Footer/>
